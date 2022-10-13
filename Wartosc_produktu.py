7196ba8f06a52cab44dad0e9d497130412676abd
Skibway <86574686+Skibway@users.noreply.github.com>
2022-10-14 01:01:11 +0200

    Summary 14.10.22
    Comparing pricing between bought and homemade products
    
    Trying to figure out the way to write to file, where need to use return

--- Wartosc_produktu.py
+++ Wartosc_produktu.py
@@ -5,20 +5,55 @@ class Compare:
         self.products = products
         self.products_price_weight = products_price_weight
 
-    def price_per_gram(self, price_per_weight):
-        if len(price_per_weight) > 1:
-            price = 0
-            for i in price_per_weight:
-                price += 
+    def price_per_gram(self, i):
+        if len(self.products_price_weight.get(i)) == 2:
+            x = self.products_price_weight.get(i)[0]
+            y = self.products_price_weight.get(i)[1]
+            return (x * 1)/y
+        else:
+            OSError            
 
-    def prod_price_per_kg(self)
+    def prod_perc_value_in_1kg(self, i):
+        kg = 1000
+        x = self.products.get(i)[0]
+        if len(self.products.get(i)) > 1:
+            for j in self.products.get(i):
+                kg *= j
+            return kg
+        elif len(self.products.get(i)) == 1:
+            kg *= x
+            return kg
+        else:
+            OSError
+
+    def summary(self):
+        a = self.price_per_weight_brutto[0]
+        b = self.price_per_weight_brutto[1]
+        sum = 0
+        list_of_prod = []
+        print("\n")
+        for i in self.products:
+            x = Compare.price_per_gram(self, i)
+            y = Compare.prod_perc_value_in_1kg(self, i)
+            sum += x * y
+            list_of_prod.append(f"{i}\t=\t:\t{round(x*y, 2)}\tfor\t{y} grams\n")
+        return f"""
+        {str(i for i in list_of_prod)}
+        {round(sum, 2)} zloty for 1000g homemade oatmeal
+        Comparing bought and homemade oatmeals
+        Bought oatmeal equals: 
+        {a} zloty for {b} grams
+        Homemade oatmeal equals: 
+        {round(sum * b / 1000, 2)} zloty for {b} grams"""
+        
+
 
 price_brutto = [11.24, 450]
 
 percentage = {
     'rolled oats': [78/100],
     'dark chocolate': [8/100],
-    'dates': [8/100, 95/100],
+    'dry dates': [8/100, 95/100],
     'rice flour': [8/100, 5/100],
     'milk chocolate': [4/100],
     'chicory inulin': [2/100]
@@ -27,10 +62,12 @@ percentage = {
 product_price_weight = {
     'rolled oats': [8.56 ,1000],
     'dark chocolate': [36.23, 100],
-    'dates': [8, 500],
+    'dry dates': [8, 500],
     'rice flour': [9.27, 500],
     'milk chocolate': [67.99, 1000],
     'chicory inulin': [15.79, 500]
 }
-
-Compare()
+with open("/Users/piotrskiba/Documents/GitHub/Comparison_Class/result.txt", "w") as f:
+    if __name__ == "__main__":
+            result = Compare(price_brutto, percentage, product_price_weight)
+            f.write(result.summary())
