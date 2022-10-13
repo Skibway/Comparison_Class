7b184b1c084d7912b48622f76d68f63ea9c0fc28
Skibway <86574686+Skibway@users.noreply.github.com>
2022-10-13 15:32:26 +0200

    Create Wartosc_produktu.py
    Beginning

---
+++ Wartosc_produktu.py
@@ -0,0 +1,36 @@
+class Compare:
+
+    def __init__(self, price_per_weight_brutto, products, products_price_weight):
+        self.price_per_weight_brutto = price_per_weight_brutto
+        self.products = products
+        self.products_price_weight = products_price_weight
+
+    def price_per_gram(self, price_per_weight):
+        if len(price_per_weight) > 1:
+            price = 0
+            for i in price_per_weight:
+                price += 
+
+    def prod_price_per_kg(self)
+
+price_brutto = [11.24, 450]
+
+percentage = {
+    'rolled oats': [78/100],
+    'dark chocolate': [8/100],
+    'dates': [8/100, 95/100],
+    'rice flour': [8/100, 5/100],
+    'milk chocolate': [4/100],
+    'chicory inulin': [2/100]
+}
+
+product_price_weight = {
+    'rolled oats': [8.56 ,1000],
+    'dark chocolate': [36.23, 100],
+    'dates': [8, 500],
+    'rice flour': [9.27, 500],
+    'milk chocolate': [67.99, 1000],
+    'chicory inulin': [15.79, 500]
+}
+
+Compare()
